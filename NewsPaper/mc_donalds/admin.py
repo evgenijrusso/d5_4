b9e95f6fab8a0f3ff8c152978adb994e82cb2c61
Rasen Nikov <rasen800@gmail.com>
2023-12-27 20:58:01 +0600

    обновление  db.sqlite3 и модели

--- NewsPaper/mc_donalds/admin.py
+++ NewsPaper/mc_donalds/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Order, Staff, ProductOrder, Country
+from .models import Product, Order, Staff, ProductOrder, Country, Author
 
 admin.site.register(Product)
 admin.site.register(ProductOrder)
 admin.site.register(Order)
 admin.site.register(Staff)
 admin.site.register(Country)
+admin.site.register(Author)
